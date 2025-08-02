20713ba1b1de3c3e96586cd8fe18c83ccbe09fae
Najm-Ur-Rehman <67186639+Najaam@users.noreply.github.com>
2025-08-01 19:37:09 -0700

    Prepeared for deployment

--- Frontend/src/lib/axios.jsx
+++ Frontend/src/lib/axios.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
-
+//in production there is nothing like locahost so we have to make it dynamic
+const Base_URL =import.meta.env.MODE === "development" ? "http://localhost:3000/api":".api"
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: Base_URL,
 });
 
 export default api;
